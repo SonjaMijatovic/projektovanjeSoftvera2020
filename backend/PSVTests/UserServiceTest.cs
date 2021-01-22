83c12fbd7bf40e59032fc572509b06cc33ea2ab6
Sonja <sonja.mijatovic@gmail.com>
2021-01-22 18:23:15 +0100

    unit tests

---
+++ backend/PSVTests/UserServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSV.Model;
+using PSV.Service;
+using Xunit.Sdk;
+
+namespace PSVTests
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void TestUserService()
+        {
+            
+            UserService service = new UserService();
+
+            Assert.IsNotNull(service);
+        }
+    }
+}
