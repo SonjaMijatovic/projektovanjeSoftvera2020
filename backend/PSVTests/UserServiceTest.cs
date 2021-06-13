95273001ed68453e300006b46b135dd2dc816ecf
Sonja Brzak <sonja.brzak@codecentric.de>
2021-06-13 21:26:08 +0200

    Added UserService Add() method test

--- backend/PSVTests/UserServiceTest.cs
+++ backend/PSVTests/UserServiceTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Moq;
 using PSV.Core;
 using PSV.Model;
@@ -36,6 +37,27 @@ namespace PSVTests
 
             var userService = new UserService(unitOfWorkMock.Object);
             Assert.False(userService.DoesUserExist(mockUserEmail));
+        }
+
+        [Fact]
+        public void GivenValidUserDataReturnsCreatedUser()
+        {
+            var mockUser = new User{Email = "user@test.com", Password = "password", FirstName = "name", LastName = "lastName"};
+            var expectedUser = new User{Email = "user@test.com", Password = "password", FirstName = "name", LastName = "lastName", UserType = "PATIENT", Blocked = false, Deleted = false, DateCreated = DateTime.Now, DateUpdated = DateTime.Now};
+
+            var userRepositoryMock = new Mock<IUserRepository>();
+            userRepositoryMock.Setup(m => m.Add(mockUser));
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(m => m.Users).Returns(userRepositoryMock.Object);
+            
+            var userService = new UserService(unitOfWorkMock.Object);
+            var addedUser = userService.Add(mockUser);
+            Assert.Equal(expectedUser.Email, addedUser.Email);
+            Assert.Equal(expectedUser.Password, addedUser.Password);
+            Assert.Equal(expectedUser.FirstName, addedUser.FirstName);
+            Assert.Equal(expectedUser.LastName, addedUser.LastName);
+            Assert.Equal(expectedUser.UserType, addedUser.UserType);
         }
     }
 }
