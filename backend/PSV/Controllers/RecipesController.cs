9cfe62abf28232c24e2191c3a33bf92175d66c44
Sonja Brzak <sonja.brzak@codecentric.de>
2021-07-02 17:01:51 +0200

    Implementing backend recipes controller functions

--- backend/PSV/Controllers/RecipesController.cs
+++ backend/PSV/Controllers/RecipesController.cs
@@ -12,18 +12,59 @@ namespace PSV.Controllers
     [ApiController]
     public class RecipesController : DefaultController
     {
+        private const String RecipesServiceBaseUrl = "http://localhost:8080/recipes/";
+
         [HttpGet]
         [Route("/api/recepies/all")]
         public async Task<ActionResult<string>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                string url = RecipesServiceBaseUrl + "all";
+                using (HttpClient client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         [HttpPost]
         [Route("/api/recepies/")]
         public async Task<HttpResponseMessage> Add(Recipe recipe)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var newRecepie = new
+                {
+                    id = -1,
+                    patient = new
+                    {
+                        firstName = recipe.Patient.FirstName,
+                        lastName = recipe.Patient.LastName,
+                    },
+                    items = new[]
+                    {
+                        new
+                        {
+                            medicine = new {name = recipe.Medicine.Name},
+                            count = recipe.Amount
+                        }
+                    }
+                };
+                using (HttpClient client = new HttpClient())
+                {
+                    return await client.PostAsync(RecipesServiceBaseUrl,
+                        new StringContent(JsonConvert.SerializeObject(newRecepie), Encoding.UTF8, "application/json"));
+                }
+            }
+            catch (Exception e)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
